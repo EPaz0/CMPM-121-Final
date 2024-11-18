c04194642a5ab7cbf0603329ded051bc0b862b14
Rmojarro1 <48000819+Rmojarro1@users.noreply.github.com>
2024-11-18 15:01:22 -0800

    Initialize deno stuff

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+let x = 0;
+
+function update() {
+  if (ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'red';
+    ctx.fillRect(x, 200, 50, 50);
+    x += 1;
+    requestAnimationFrame(update);
+  }
+};
+
+update();
