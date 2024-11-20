67a91b3fe28b09fa33cf1fde5c35179b56d93df5
Rmojarro1 <48000819+Rmojarro1@users.noreply.github.com>
2024-11-20 13:12:45 -0800

    Add HTML element that displays the information of the current cell in the bottom right of the screen

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 const canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-//let x = 0;
-
 const playerCoordinates = {col: 0, row: 0}; 
 
 interface Cell{
@@ -23,7 +21,7 @@ const grid: Cell[][] = Array.from({ length: rows }, (_, rowIndex) =>
 
 function drawCell(ctx: CanvasRenderingContext2D, cell: Cell) {
   if(ctx){
-    ctx.fillStyle = 'green';
+    ctx.fillStyle = 'black';
     ctx.strokeRect(cell.x, cell.y, gridSize, gridSize);
   }
 }
@@ -37,6 +35,15 @@ function drawPlayer(ctx: CanvasRenderingContext2D) {
   const cellX = playerCoordinates.col * gridSize;
   const cellY = playerCoordinates.row * gridSize;
   ctx.fillRect(cellX, cellY, gridSize, gridSize);
+}
+
+function updateCellInfo(cell: Cell) {
+  const cellInfoDiv = document.getElementById('cell-info');
+  if (cellInfoDiv) {
+    cellInfoDiv.innerText = `Cell Info:
+Row: ${playerCoordinates.row}
+Col: ${playerCoordinates.col}`;
+  }
 }
 
 function playerMovement(event: KeyboardEvent) {
@@ -70,6 +77,8 @@ function playerMovement(event: KeyboardEvent) {
     playerCoordinates.row = newRow;
     playerCoordinates.col = newCol;
   }
+  const currentCell = grid[newRow][newCol];
+  updateCellInfo(currentCell);
 }
 document.addEventListener('keydown', playerMovement);
 
@@ -83,4 +92,5 @@ function update() {
   }
 };
 
+updateCellInfo(grid[playerCoordinates.row][playerCoordinates.col]);
 update();
