8d31634e98620e8b0438fb3784c626c76bc1efdf
EPaz0 <enrique_pazg@yahoo.com>
2024-12-08 13:25:34 -0800

    F3.c & d done

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import {fileURLToPath, URL} from 'node:url'
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import {VitePWA} from 'vite-plugin-pwa' 
+
+export default defineConfig({
+    plugins: [
+        vue(), 
+        VitePWA({
+            manifest: {
+                name: "Fish Farm",
+                short_name: "FishFarm",
+                description: "A fun fish farming game!",
+                icons: [
+                    {
+                        src: "/icons/fish.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                       
+                    },
+                ],
+                start_url: "/",
+                display: "standalone",
+                background_color: "#ffffff",
+                theme_color: "#4CAF50",
+            },
+        }),
+    ],
+    build: {
+        outDir: '../wwwroot',
+        emptyOutDir: true,
+    },
+    resolve: {
+        alias : {   
+            '@': fileURLToPath(new URL('src', import.meta.url)),
+        },
+    },
+})
